2b430beeba1cb2d9500617f4da0b7d41f2210975
phatle21 <patrick.le.204@gmail.com>
2025-10-10 14:57:42 +0700

    test change code on learning_selenium branch.

---
+++ src/main/java/org/seleniumhowto/test_architect_pom/utils/ExtentReportManager.java
@@ -0,0 +1,18 @@
+package org.seleniumhowto.test_architect_pom.utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportManager {
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+        }
+        return extent;
+    }
+}
