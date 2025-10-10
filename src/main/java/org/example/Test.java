5f81a3a7e078239e5d0cfb82e3f5ffef7590cc17
phatle21 <patrick.le.204@gmail.com>
2025-10-10 11:00:20 +0700

    test push code on practice_coding branch.

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!~~");
+        System.out.println("This a class test");
     }
 }
